8cd8b446a529c7aadf3446bb434f946abfb84a65
Talon Ezio <vuthemanh1707@gmail.com>
2024-04-24 00:32:26 +0700

    Add project files.

---
+++ SuperEncode.Wpf/Converters/FontFamilyNameConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace SuperEncode.Wpf.Converters
+{
+    public class FontFamilyNameConverter: IValueConverter
+    {
+        public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is not FontFamily input) return "No Name Font";
+
+
+            var fontName = input.Source.Split("#")[^1];
+            return fontName;
+
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SuperEncode.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace SuperEncode.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string _currentDirectory = Environment.CurrentDirectory;
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadFonts();
+
+        }
+        private void LoadFonts()
+        {
+            var installedFonts = Fonts.GetFontFamilies("C:\\Windows\\Fonts")
+                .Where(x =>
+                {
+                    var fontName = x.Source.Split("#")[^1];
+                    return fontName.StartsWith("UTM") || fontName.StartsWith("UVF") || fontName.StartsWith("UVN");
+                });
+
+            CmbFont.ItemsSource = installedFonts;
+        }
+        private void BtnSelectPath_Click(object sender, RoutedEventArgs e)
+        {
+            var folderDialog = new OpenFolderDialog();
+            folderDialog.ShowDialog();
+
+            _currentDirectory = folderDialog.FolderName;
+
+            TxtPath.Text = _currentDirectory;
+        }
+    }
+}
